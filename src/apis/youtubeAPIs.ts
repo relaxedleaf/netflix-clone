5367f4f924f46db2279b7701a83d6de1517fb629
Guanghui Li <liguanghui.cs@gmail.com>
2023-02-11 17:11:49 -0600

    formatted the url

--- src/apis/youtubeAPIs.ts
+++ src/apis/youtubeAPIs.ts
@@ -1,10 +1,18 @@
 import Video from '@/types/Video';
 import YoutubeSearchResponse from '@/types/YoutubeSearchResponse';
+import { getUrl } from '@/utils/fetchUtils';
 
 export const fetchVideos = async (search: string): Promise<Array<Video>> => {
 	const response = await fetch(
-		`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search}&key=${process
-			.env.YOUTUBE_API_KEY!}`,
+		getUrl({
+			baseUrl: 'https://youtube.googleapis.com/youtube/v3/search',
+			params: {
+				part: 'snippet',
+				maxResults: 25,
+				q: 'search',
+				key: process.env.YOUTUBE_API_KEY!,
+			},
+		}),
 		{
 			cache: 'no-store',
 		}
